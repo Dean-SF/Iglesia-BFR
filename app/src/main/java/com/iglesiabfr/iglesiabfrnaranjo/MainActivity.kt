845beb5b2a05a1b39e6f3b286a56e05a26cac2d7
Dean-SF <dsanabriaf@estudiantec.cr>
2024-04-04 17:25:02 -0600

    Conexion a base de datos

--- app/src/main/java/com/iglesiabfr/iglesiabfrnaranjo/MainActivity.kt
+++ app/src/main/java/com/iglesiabfr/iglesiabfrnaranjo/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.iglesiabfr.iglesiabfrnaranjo
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -15,14 +16,8 @@ import com.iglesiabfr.iglesiabfrnaranjo.ui.theme.IglesiaBFRNaranjoTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            IglesiaBFRNaranjoTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
-            }
-        }
+        val i =  Intent(this,Testingdb::class.java)
+        startActivity(i)
     }
 }
 
