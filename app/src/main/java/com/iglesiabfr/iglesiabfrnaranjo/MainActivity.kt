b773950d9329aa20a77dc215aabc9c41014c0cc5
Erick <efmz2000@outlook.es>
2024-04-05 23:55:16 -0600

    Daily vers test

--- app/src/main/java/com/iglesiabfr/iglesiabfrnaranjo/MainActivity.kt
+++ app/src/main/java/com/iglesiabfr/iglesiabfrnaranjo/MainActivity.kt
@@ -3,10 +3,6 @@ package com.iglesiabfr.iglesiabfrnaranjo
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -16,7 +12,8 @@ import com.iglesiabfr.iglesiabfrnaranjo.ui.theme.IglesiaBFRNaranjoTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val i =  Intent(this,Testingdb::class.java)
+//        val i =  Intent(this,Testingdb::class.java)
+        val i =  Intent(this,DailyVers::class.java)
         startActivity(i)
     }
 }
