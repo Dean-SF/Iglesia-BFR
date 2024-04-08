caef16afeb34535167291f272e3b5be8a8d52377
iHuskyx <andrealh17@estudiantec.cr>
2024-04-08 13:19:42 -0600

    Pantallas de inicio, login y registro
    Sin funcionalidad, solo pantallas

---
+++ app/src/main/java/com/iglesiabfr/iglesiabfrnaranjo/RegistrationActivity.kt
@@ -0,0 +1,26 @@
+package com.iglesiabfr.iglesiabfrnaranjo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RegistrationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+
+//        val registerBtn: Button = findViewById(R.id.registerBtn)
+//        registerBtn.setOnClickListener {
+//            callLogin()
+//        }
+    }
+
+    // Llamada de ventanas
+//    private fun callLogin(){
+//        val intent = Intent(this, LoginActivity::class.java)
+//        startActivity(intent)
+//    }
+//
+//    private fun registrateUser(){
+//        TO-DO
+//    }
+}
