f5c7c4c28c6b998724e34b59adeb76ac44d29add
Erick <efmz2000@outlook.es>
2024-04-08 15:49:39 -0600

    Cambio de los requests

---
+++ app/src/main/java/com/iglesiabfr/iglesiabfrnaranjo/BibleBooks.kt
@@ -0,0 +1,20 @@
+package com.iglesiabfr.iglesiabfrnaranjo
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class BibleBooks : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_bible_books)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
