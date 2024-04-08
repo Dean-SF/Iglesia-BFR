bd508cb0aa92be0ce282c831dcb2aa315f6d54d8
Dean-SF <dsanabriaf@estudiantec.cr>
2024-04-08 16:41:43 -0600

    Fix homepage

--- app/src/main/java/com/iglesiabfr/iglesiabfrnaranjo/Homepage.kt
+++ app/src/main/java/com/iglesiabfr/iglesiabfrnaranjo/Homepage.kt
@@ -11,12 +11,6 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 class Homepage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_homepage)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
