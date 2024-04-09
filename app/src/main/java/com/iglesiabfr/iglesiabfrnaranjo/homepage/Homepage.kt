a4f0515f991c18b76a59ba35bde6a54fa951dc13
Dean-SF <dsanabriaf@estudiantec.cr>
2024-04-08 20:08:43 -0600

    Esqueleto admin

--- app/src/main/java/com/iglesiabfr/iglesiabfrnaranjo/homepage/Homepage.kt
+++ app/src/main/java/com/iglesiabfr/iglesiabfrnaranjo/homepage/Homepage.kt
@@ -1,4 +1,4 @@
-package com.iglesiabfr.iglesiabfrnaranjo.Homepage
+package com.iglesiabfr.iglesiabfrnaranjo.homepage
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
--- app/src/main/java/com/iglesiabfr/iglesiabfrnaranjo/login/LoginActivity.kt
+++ app/src/main/java/com/iglesiabfr/iglesiabfrnaranjo/login/LoginActivity.kt
@@ -4,7 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.iglesiabfr.iglesiabfrnaranjo.Homepage.Homepage
+import com.iglesiabfr.iglesiabfrnaranjo.homepage.Homepage
 import com.iglesiabfr.iglesiabfrnaranjo.R
 
 class LoginActivity : AppCompatActivity() {
