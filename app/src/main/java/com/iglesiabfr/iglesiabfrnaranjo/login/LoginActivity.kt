3dba7f8540b9095f494daa7cf7e4abb0a1b57d5d
iHuskyx <andrealh17@estudiantec.cr>
2024-04-28 13:35:44 -0600

    correccion login

--- app/src/main/java/com/iglesiabfr/iglesiabfrnaranjo/login/LoginActivity.kt
+++ app/src/main/java/com/iglesiabfr/iglesiabfrnaranjo/login/LoginActivity.kt
@@ -4,12 +4,13 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.iglesiabfr.iglesiabfrnaranjo.R
+import com.iglesiabfr.iglesiabfrnaranjo.database.DatabaseConnector
 import com.iglesiabfr.iglesiabfrnaranjo.homepage.Homepage
-import io.realm.kotlin.Realm
 import io.realm.kotlin.mongodb.App
 import io.realm.kotlin.mongodb.Credentials
 import io.realm.kotlin.mongodb.User
@@ -18,16 +19,21 @@ import kotlinx.coroutines.launch
 class LoginActivity : AppCompatActivity() {
 
     private val app : App = App.create("iglesiabfr-pigqi")
-    private val db : App = App.create("iglesiabfr-pigqi")
-    private lateinit var realm : Realm
     private var user : User? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        DatabaseConnector.connect() // Conectar a bd
+
         val loginBtn: Button = findViewById(R.id.loginBtn)
         loginBtn.setOnClickListener {
+            checkInputs()
+        }
+
+        val forgotPasswordText: TextView = findViewById(R.id.forgotPassTxt)
+        forgotPasswordText.setOnClickListener {
             checkInputs()
         }
     }
@@ -57,10 +63,32 @@ class LoginActivity : AppCompatActivity() {
             if (user == null) {
                 Toast.makeText(this@LoginActivity, R.string.incorrectDataWarning, Toast.LENGTH_SHORT).show()
             } else {
+                Toast.makeText(this@LoginActivity, DatabaseConnector.getLogCurrent().id, Toast.LENGTH_SHORT).show()
                 callMainMenu()
             }
         }
     }
+
+    private suspend fun forgotPassword(email: String) {
+        try {
+
+            // Get the EmailPasswordAuth instance
+            val emailPasswordAuth = user?.app?.emailPasswordAuth
+
+            // Send reset password email
+            emailPasswordAuth?.sendResetPasswordEmail(email)?.await().onSuccess {
+                // Éxito al enviar el correo electrónico de restablecimiento de contraseña
+                Toast.makeText(this@LoginActivity, R.string.resetPasswordEmailSent, Toast.LENGTH_SHORT).show()
+            }?.onFailure { error ->
+                // Error al enviar el correo electrónico de restablecimiento de contraseña
+                Toast.makeText(this@LoginActivity, "Error: ${error.message}", Toast.LENGTH_SHORT).show()
+            }
+        } catch (e: Exception) {
+            // Manejar cualquier excepción que pueda ocurrir durante el proceso
+            Toast.makeText(this@LoginActivity, "Error: ${e.message}", Toast.LENGTH_SHORT).show()
+        }
+    }
+
 
     private suspend fun login(emailInput: String, passwordInput: String): User? {
         return try {
