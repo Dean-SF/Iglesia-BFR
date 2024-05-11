888dec0b01234a78dab426e415468f2e97f8439a
Steven Alvarado Aguilar <alvaradosteven6@gmail.com>
2024-05-10 19:21:32 -0600

    Nuevos cambios

--- app/src/main/java/com/iglesiabfr/iglesiabfrnaranjo/admin/events/AdminEvent.kt
+++ app/src/main/java/com/iglesiabfr/iglesiabfrnaranjo/admin/events/AdminEvent.kt
@@ -6,6 +6,7 @@ import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.iglesiabfr.iglesiabfrnaranjo.R
 import com.iglesiabfr.iglesiabfrnaranjo.database.DatabaseConnector
+import com.iglesiabfr.iglesiabfrnaranjo.homepage.Adminpage
 import com.iglesiabfr.iglesiabfrnaranjo.schema.Attendance
 import io.realm.kotlin.Realm
 import io.realm.kotlin.types.RealmInstant
@@ -27,6 +28,12 @@ class AdminEvent : AppCompatActivity() {
         markAttendanceButt.setOnClickListener {
             val i = Intent(this,CreateEvent::class.java)
             startActivity(i)
+        }
+
+        val backBtn: Button = findViewById(R.id.backAdminEventButton)
+        backBtn.setOnClickListener {
+            val intent = Intent(this, Adminpage::class.java)
+            startActivity(intent)
         }
     }
 }
