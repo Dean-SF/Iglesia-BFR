d2d8ba16062c2148c7e89f1d79ede029ccc9a2e3
Dean-SF <dsanabriaf@estudiantec.cr>
2024-05-15 14:12:40 -0600

    Registrar bugfix, entrar automatico al reset password cuando se ingresa al link

--- app/src/main/java/com/iglesiabfr/iglesiabfrnaranjo/login/ResetPasswordSendEmail.kt
+++ app/src/main/java/com/iglesiabfr/iglesiabfrnaranjo/login/ResetPasswordSendEmail.kt
@@ -9,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.iglesiabfr.iglesiabfrnaranjo.R
 import com.iglesiabfr.iglesiabfrnaranjo.database.AppConnector
+import com.iglesiabfr.iglesiabfrnaranjo.dialogs.LoadingDialog
 import io.realm.kotlin.mongodb.App
 import io.realm.kotlin.mongodb.User
 import kotlinx.coroutines.launch
@@ -19,16 +20,20 @@ class ResetPasswordSendEmail : AppCompatActivity() {
     private lateinit var emailInput: EditText
     private val app: App = AppConnector.app
     private var user: User? = null
+    private lateinit var loadingDialog : LoadingDialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reset_password_email)
+
+        loadingDialog = LoadingDialog(this)
 
         user = app.currentUser
         emailInput = findViewById(R.id.inputEmail)
 
         val resetPasswordButton: Button = findViewById(R.id.resetBtn)
         resetPasswordButton.setOnClickListener {
+            loadingDialog.startLoading()
             lifecycleScope.launch {
                 checkInputs()
             }
@@ -39,9 +44,11 @@ class ResetPasswordSendEmail : AppCompatActivity() {
         email = emailInput.text.toString()
 
         if (email.isEmpty()) {
+            loadingDialog.stopLoading()
             Toast.makeText(this, "Los datos están incompletos", Toast.LENGTH_LONG).show()
         } else {
             app.emailPasswordAuth.sendResetPasswordEmail(email)
+            loadingDialog.stopLoading()
             val intent = Intent(this@ResetPasswordSendEmail, ResetPassword::class.java)
             intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
             startActivity(intent)
