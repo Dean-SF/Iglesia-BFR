4f62f43f22b7255e1ac172003eee3be5ea9dea45
Alison Barrantes Canales <abarrantescanales@gmail.com>
2024-05-23 18:36:46 -0600

    Cambio
    Cambio el formato de la fecha para que se vea en día. mes. año y la hora de creación

--- app/src/main/java/com/iglesiabfr/iglesiabfrnaranjo/admin/adminSchoolMaterial/SchoolMaterialViewHolder.kt
+++ app/src/main/java/com/iglesiabfr/iglesiabfrnaranjo/admin/adminSchoolMaterial/SchoolMaterialViewHolder.kt
@@ -4,20 +4,26 @@ import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.iglesiabfr.iglesiabfrnaranjo.databinding.ItemSchoolMaterialListBinding
 import com.iglesiabfr.iglesiabfrnaranjo.schema.SchoolMaterial
+import java.time.LocalDateTime
+import java.time.ZoneOffset
+import java.time.format.DateTimeFormatter
 
 class SchoolMaterialViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
     private val binding = ItemSchoolMaterialListBinding.bind(view)
+    private val formatter = DateTimeFormatter.ofPattern("dd/MM/yy")
 
     fun render(
         schoolMaterialModel: SchoolMaterial,
         onClickListener: (SchoolMaterial) -> Unit,
         onClickDelete: (Int) -> Unit
     ) {
+        val initial = LocalDateTime.ofEpochSecond(schoolMaterialModel.initialMonth.epochSeconds, 0, ZoneOffset.UTC)
+        val final = LocalDateTime.ofEpochSecond(schoolMaterialModel.finalMonth.epochSeconds, 0, ZoneOffset.UTC)
         binding.tvTeacher.text = schoolMaterialModel.teacherName
         binding.tvClase.text = schoolMaterialModel.clase
-        binding.etInitialMonth.text = schoolMaterialModel.initialMonth.toString()
-        binding.etFinalMonth.text = schoolMaterialModel.finalMonth.toString()
+        binding.etInitialMonth.text = initial.format(formatter)
+        binding.etFinalMonth.text = final.format(formatter)
 
         itemView.setOnClickListener { onClickListener(schoolMaterialModel) }
         binding.btnDelete.setOnClickListener { onClickDelete(absoluteAdapterPosition) }
