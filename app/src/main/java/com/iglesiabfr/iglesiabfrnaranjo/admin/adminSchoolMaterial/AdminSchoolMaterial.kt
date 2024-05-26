fff058c141e905bf6050be5614f52926894e052c
Dean-SF <dsanabriaf@estudiantec.cr>
2024-05-26 08:00:39 -0600

    School material fixed

--- app/src/main/java/com/iglesiabfr/iglesiabfrnaranjo/admin/adminSchoolMaterial/AdminSchoolMaterial.kt
+++ app/src/main/java/com/iglesiabfr/iglesiabfrnaranjo/admin/adminSchoolMaterial/AdminSchoolMaterial.kt
@@ -1,5 +1,6 @@
 package com.iglesiabfr.iglesiabfrnaranjo.admin.adminSchoolMaterial
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
@@ -35,13 +36,16 @@ class AdminSchoolMaterial : AppCompatActivity() {
     private lateinit var binding: ActivityAddSchoolMaterialAdminBinding
     private lateinit var binding1: ActivitySchoolMaterialAdminBinding
     private lateinit var adapter: SchoolMaterialAdapter
+    private var currentBinding = 0
     private val llmanager = LinearLayoutManager(this)
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAddSchoolMaterialAdminBinding.inflate(layoutInflater)
         binding1 = ActivitySchoolMaterialAdminBinding.inflate(layoutInflater)
         setContentView(binding1.root)
+        currentBinding = 0
 
         realm = DatabaseConnector.db
 
@@ -100,19 +104,28 @@ class AdminSchoolMaterial : AppCompatActivity() {
         binding1.btnAddSchoolMaterial.setOnClickListener {
             // Set content view to binding1 after adding inventory schoolMaterial
             setContentView(binding.root)
+            currentBinding = 1
         }
 
         binding.btnAddSchoolMaterial.setOnClickListener {
             createSchoolMaterial()
         }
 
-        binding.BackAddSchoolMaterialButton.setOnClickListener {
-            setContentView(binding1.root)
-            loadSchoolMaterial() // Asegúrate de cargar los profesores al volver
-        }
-
         initRecyclerView()
         loadSchoolMaterial() // Cargar los videos al inicio
+    }
+
+    @Deprecated("Deprecated in Java",
+        ReplaceWith("super.onBackPressed()", "androidx.appcompat.app.AppCompatActivity")
+    )
+    override fun onBackPressed() {
+        if(currentBinding == 1) {
+            currentBinding = 0
+            setContentView(binding1.root)
+            loadSchoolMaterial()
+            return
+        }
+        super.onBackPressed()
     }
 
     private fun createSchoolMaterial() {
@@ -195,16 +208,11 @@ class AdminSchoolMaterial : AppCompatActivity() {
 
     private fun initRecyclerView(){
         adapter = SchoolMaterialAdapter(
-            onClickListener = { schoolMaterial: SchoolMaterial -> onItemSelected(schoolMaterial) },
+            onClickListener = null,
             onClickDelete = { position: Int -> onDeletedItem(position) }
         )
         binding1.recyclerSchoolMaterial.layoutManager = llmanager
         binding1.recyclerSchoolMaterial.adapter = adapter
-    }
-
-    private fun onItemSelected(schoolMaterial: SchoolMaterial) {
-        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(schoolMaterial.teacherName))
-        startActivity(intent)
     }
 
     private fun onDeletedItem(position: Int) {
