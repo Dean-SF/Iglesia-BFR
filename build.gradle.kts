2d22351bac89ef2ed70bb86905eefc08e4106a8f
Erick <efmz2000@outlook.es>
2024-05-25 23:26:41 -0600

    Config de sonar
    ./gradlew sonar -D "sonar.projectKey=igleasiaBFR" -D "sonar.projectName=igleasiaBFR" -D "sonar.host.url=http://localhost:9000" -D "sonar.token=sqp_36e9f663c2b6ad432563c2c95ec48929d2443afc"

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,5 +4,13 @@ plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
     alias(libs.plugins.googleServicesPlug) apply false
+    id("org.sonarqube") version "5.0.0.4638"
 }
 
+sonar {
+    properties {
+        property("sonar.projectKey", "igleasiaBFR")
+        property("sonar.organization", "myOrganization")
+        property("sonar.host.url", "http://localhost:9000")
+    }
+}
